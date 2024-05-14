81dd7c15ed2d6d04733f83f62c1f5884cffb5528
kangddan <125175728+kangddan@users.noreply.github.com>
2024-05-14 14:58:54 +0800

    Update copyPointWeight.py

--- copyPointWeight.py
+++ copyPointWeight.py
@@ -3,40 +3,45 @@ from c4d import gui
 doc = c4d.documents.GetActiveDocument()
 
 class CopyPointWeightUI(gui.GeDialog):
+    MAINLAYOUTID = 41650
+    LINKBOXID = 19165
+    TEXTEDITID = 41915
+    COPYBUTID = 123475
+    PASTEBUTID = 16546
+
     UI_INSTANCE = None
     @classmethod
     def UIDisplay(cls):
-        if not cls.UI_INSTANCE:
+        if cls.UI_INSTANCE is None:
             cls.UI_INSTANCE = CopyPointWeightUI()
-        if not cls.UI_INSTANCE.IsVisible():
-            cls.UI_INSTANCE.Open(dlgtype=c4d.DLG_TYPE_ASYNC, pluginid=0, xpos=-2, ypos=-2,
-                                 defaultw=300, defaulth=100, subid=0)
         else:
             cls.UI_INSTANCE.Open(dlgtype=c4d.DLG_TYPE_ASYNC)
 
     def __init__(self, *args, **kwargs):
         super(CopyPointWeightUI, self).__init__(*args, **kwargs)
+        self.Open(dlgtype=c4d.DLG_TYPE_ASYNC, pluginid=0,
+                  xpos=-1, ypos=-1, defaultw=300, defaulth=100, subid=0)
         self.weights = []
 
     def CreateLayout(self):
         self.SetTitle('Copy Point Weight')
-        self.mainLayout(self.createBut)
+        self.mainLayout(self.createWidget)
         self.GroupEnd()
         return True
 
     # ---------------------------------------------------------
 
     def mainLayout(self, widgets):
-        vHBLayout = self.GroupBegin(7777, flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT,
-                         cols=0, rows=0, title="", groupflags=0, initw=0, inith=0)
+        self.GroupBegin(self.MAINLAYOUTID, flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT, cols=2, rows=0, title="",
+                        groupflags=c4d.BFV_GRIDGROUP_EQUALCOLS, initw=0, inith=0)
         widgets()
-        self.GroupEnd()
 
-    def createBut(self):
-        self.copyBut = self.AddButton(8888, flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT,
-                       initw=100 ,inith=30 ,name='Copy')
-        self.pasteBut = self.AddButton(9999, flags=c4d.BFH_SCALEFIT|c4d.BFV_SCALEFIT,
-                      initw=100 ,inith=30 ,name='Paste')
+    def createWidget(self):
+        self.AddCustomGui(self.LINKBOXID, c4d.CUSTOMGUI_LINKBOX, 'Link Object', c4d.BFH_SCALEFIT, 100, 0)
+        self.AddEditText(self.TEXTEDITID, flags=c4d.BFH_SCALEFIT, initw=0, inith=0, editflags=0)
+
+        self.copyBut = self.AddButton(self.COPYBUTID, flags=c4d.BFH_SCALEFIT, initw=100 ,inith=30 ,name='Copy')
+        self.pasteBut = self.AddButton(self.PASTEBUTID, flags=c4d.BFH_SCALEFIT, initw=100 ,inith=30 ,name='Paste')
 
     # ---------------------------------------------------------
 
@@ -51,10 +56,15 @@ class CopyPointWeightUI(gui.GeDialog):
             c4d.gui.MessageDialog('The selected object has no weight tag!')
             return True
 
-        if _id == 8888:
+        if _id == self.COPYBUTID:
             self.weights = []
-            self.getSelectedPointWeight(obj, weightTag, self.weights)
-        elif _id == 9999:
+            _str = self.getSelectedPointWeight(obj, weightTag, self.weights)
+
+            self.SetString(self.TEXTEDITID, _str, tristate=False, flags=0)
+            linkBox = self.FindCustomGui(self.LINKBOXID, c4d.CUSTOMGUI_LINKBOX)
+            linkBox.SetLink(obj)
+
+        elif _id == self.PASTEBUTID:
             doc.StartUndo()
             doc.AddUndo(c4d.UNDOTYPE_CHANGE, obj)
             self.setSelectedPointsWeight(obj, weightTag, self.weights)
@@ -69,7 +79,7 @@ class CopyPointWeightUI(gui.GeDialog):
             for j in range(weightTag.GetJointCount()):
                 pointWeight = weightTag.GetWeight(j, p)
                 weightData.append(pointWeight)
-            break
+            return '< {} > :{}'.format('selected point ID', p)
 
     @staticmethod
     def setSelectedPointsWeight(obj, weightTag, weightData):
@@ -92,8 +102,8 @@ class CopyPointWeightUI(gui.GeDialog):
 if __name__=='__main__':
     CopyPointWeightUI.UIDisplay()
 
-'''
+"""
 code by kangddan
 Revision History
 Revision 1: 2024-04-23 : First publish
-'''
+"""
